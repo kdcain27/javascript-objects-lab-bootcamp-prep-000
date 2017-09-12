d998e1f2e698b64e1e4530369bd864b87b8b120a
Kevin Krause <kkrausesc@gmail.com>
2017-09-12 22:10:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
+  var  object = Object.assign({},object, key)
   delete object.key
   return object;
 }
