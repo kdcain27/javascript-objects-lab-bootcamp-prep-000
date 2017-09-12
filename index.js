f2e4997ef6fd28b6269e81ea9973c35dbc3f5724
Kevin Krause <kkrausesc@gmail.com>
2017-09-12 22:41:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function deleteFromObjectByKey(object, key) {
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   var obj = {key: 'value'}
-  delete object.key
+  delete obj.key
   return obj;
 }
